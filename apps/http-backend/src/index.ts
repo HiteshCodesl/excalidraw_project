8311723135837de51da956d678df9f48fc94f315
Hitesh Navare <navarehitesh65@gmail.com>
2025-05-08 14:13:58 +0530

    added db

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -2,16 +2,33 @@ import express from "express";
 import jwt from "jsonwebtoken";
 import { JWT_SECRET } from "@repo/backend-commen/config";
 import { middleware } from "./middleware";
+import {CreateUserSchema, SigninSchema, CreateRoomSchema} from  
+"@repo/commen/types"
+import {prismaClient} from "@repo/db/client"
 
 const app = express();
 
 app.post("signup", (req, res)=>{
+    const data = CreateUserSchema.safeParse(req.body);
+    if(!data.success){
+        res.json({
+            message: "Incorrect inputs"
+        })
+        return;
+    }
     res.json({
         userId: "123"
     })
 })
 
 app.post("signin", async(req, res)=>{
+    const data = SigninSchema.safeParse(req.body);
+    if(!data.success){
+        res.json({
+            message: "Incorrect inputs"
+        })
+        return;
+    }
     
 const userId = 1;
     const token = await jwt.sign({
